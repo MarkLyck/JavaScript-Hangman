f9708910b44b25664f8bd263c5db0fe429185d14
Mark Lyck <mark.lyck@gmail.com>
2016-06-28 17:28:05 -0500

    Added sounds, styled it, added modal panels, added GET request for random word, added button layout, and cleaned up code

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
 var d = document;
 var word = '';
 var displayArray = [];
@@ -6,83 +14,144 @@ var guessedChars = [];
 var correctChars = [];
 var turnsRemaining = d.querySelector('#turnsRemaining');
 var guessWord = d.querySelector('#guessWord');
-var input = d.querySelector('input');
+var guessedContainer = d.querySelector('#guessed-container');
+var harhar = new Audio('assets/sounds/Nelson.mp3');
+var winCounter = 0;
+var lossCounter = 0;
+var newGameBtn = d.querySelector('#newGame');
+var modalContainer = d.querySelector('.modal-container');
+var modalTitle = d.querySelector('.modal h3');
+
+newGameBtn.addEventListener('click', function(){
+  modalContainer.style.display = 'none';
+  newGame();
+});
+
+var letters = d.querySelectorAll('.letter');
+
+function guess(e) {
+
+  if (guessedChars.indexOf(e.target.innerHTML) === -1) {
+    guessedChars.push(e.target.innerHTML);
+    // e.target.style.background = 'red';
+    e.target.classList.add('guessed');
+    testInput(e.target.innerHTML);
+  } else {
+    console.log('This character has already been guessed');
+  }
+}
+
+letters.forEach(function(letter) {
+  letter.addEventListener('click', guess);
+});
 
 newGame();
 function newGame() {
   var randomNumber = Math.round(Math.random()*commonWords.length);
-  word = commonWords[randomNumber];
-  // Set up underscores
+  // word = commonWords[randomNumber].toUpperCase();
+  var saveWord = httpGet('http://randomword.setgetgo.com/get.php');
+  console.log(saveWord);
+  word = saveWord.toUpperCase();
+
   displayArray = [];
   word.split('').forEach(function(char){
-    //Using regex allows for sentences to have commas, and periods (for more advanced hangman)
     displayArray.push('_');
-    // guessWord.textContent = word.replace(/[a-zA-Z]/g, '_ ');
   });
   guessWord.textContent = displayArray.join(' ');
   turns = 10;
   console.log('New word = ' + word);
+
+  letters.forEach(function(letter) { // Reset letter styling
+    letter.classList.remove('guessed');
+  });
+
+  guessedChars = [];
+  updateGuesses();
+  updateTurns();
 }
 
-input.addEventListener('keyup', function(e){
-  if (e.keyCode === 13) {
-    testInput(input.value);
-  }
-});
+
+
 
 function testInput(guess) {
-  input.value = '';
-  if (guess.length > 1) { // If user is trying to guess the entire word
-    turns--;
-    if (guess === word) {
-      console.log('YOU WON!');
-    }
-  } else if (guess.length === 0) { // If nothing was typed in
+
+  console.log(guess);
+  if (guess.length === 0) { // If nothing was typed in
     throw new Error('You didn\'t type in anything');
-
-  } else if (guessedChars.indexOf(guess) !== -1) { // If character was already guessed
-    console.log('You have already guessed this character');
-
   } else if (word.indexOf(guess) !== -1) { // If the player guesses a correct character
-    turns--;
     console.log('Correct char guess');
-    guessedChars.push(guess);
-
     displayArray = []; // Reset displayArray
     word.split('').forEach(function(char) {
       if (guessedChars.indexOf(char) !== -1) {
+
         displayArray.push(char); // Add guessed chars to array
       } else {
         displayArray.push('_'); // Add underscores for not yet guessed chars
       }
     });
-
     guessWord.textContent = displayArray.join(' '); // Update p tag
-
-    console.log(displayArray.indexOf('_'));
     if (displayArray.indexOf('_') === -1) { // If all letters have been guessed
       console.log('YOU WON!');
+      win();
     }
-
-  } else {
+  } else if (turns !== 1) {
     turns--;
     console.log('Wrong');
-  }
-
-  updateTurns();
-
-  if (turns === 0) {
+  } else {
     console.log('YOU LOST!');
-    newGame();
-    updateTurns();
+    loss();
   }
+  updateGuesses();
+  updateTurns();
 }
+
+
+
+
+
 
 function updateTurns() {
   if (turns !== 1) {
-    turnsRemaining.textContent = turns + ' turns left';
+    turnsRemaining.textContent = turns + ' turns remaining';
   } else {
-    turnsRemaining.textContent = turns + ' turn left';
+    turnsRemaining.textContent = turns + ' turn remaining';
   }
+}
 
+
+
+
+
+
+
+function updateGuesses() {
+  while (guessedContainer.firstChild) {
+    guessedContainer.removeChild(guessedContainer.firstChild);
+  }
+  guessedChars.forEach(function(guess) {
+    var node = document.createElement("li");
+    var textNode = document.createTextNode(guess);
+    node.appendChild(textNode);
+    guessedContainer.appendChild(node);
+  });
+}
+
+
+function win() {
+  modalContainer.style.display = 'flex';
+  modalTitle.style.color = 'rgb(55, 221, 166)';
+  newGameBtn.style.background = 'rgb(55, 221, 166)';
+  modalTitle.textContent = 'You Won!';
+  winCounter++;
+  updateTurns();
+}
+
+function loss() {
+  harhar.play();
+  modalContainer.style.display = 'flex';
+  modalTitle.style.color = '#e74c3c';
+  newGameBtn.style.background = '#e74c3c';
+  modalTitle.textContent = 'You Lost!';
+  lossCounter++;
+  updateTurns();
 }
